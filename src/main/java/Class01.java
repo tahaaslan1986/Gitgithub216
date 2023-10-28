b5c29d4140224a2c7657cc1225e1739b3738e5c6
Taha Aslan <eng.tahaaslan@gmail.com>
2023-10-28 20:39:51 +0300

    V1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,15 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        // version 1 için kodlar
+
+    int x = 1 ;
+    int y = 2 ;
+
+        System.out.println(x+y);
+        System.out.println("Version 1 tamamlandı.");
+
+
+
+    }
+}
